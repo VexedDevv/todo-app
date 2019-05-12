90ada6992cdcb397abb67d6846a215bad8ca3756
Dustin Curdt <dustin@vexeddev.net>
2019-05-12 19:55:22 +0200

    introduce index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/********************************************************************************
+ *     Urheberrechtshinweis                                                     *
+ *     Copyright © Dustin Curdt 2019                                            *
+ *     Erstellt: 12.05.2019 / 19:43                                             *
+ *                                                                              *
+ *     Alle Inhalte Dieses Quelltextes sind urheberrechtlich geschützt.         *
+ *     Das Urheberrecht liegt, soweit nicht ausdrücklich anders gekennzeichnet, *
+ *     bei Dustin Curdt. Alle Rechte vorbehalten.                               *
+ *                                                                              *
+ *     Jede Art der Vervielfältigung, Verbreitung, Vermietung, Verleihung       *
+ *     öffentlichen Zugänglichmachung oder andere Nutzung                       *
+ *     bedarf der ausdrücklichen, schriftlichen Zustimmung von Dustin Curdt     *
+ ********************************************************************************/
+$t = "my";
+echo "Hello $t World \n";
+echo 'Hello $t World \n';
