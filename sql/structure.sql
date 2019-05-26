349ab9d90bb72fb71139f605428325e6630f205c
Dustin Curdt <dustin@vexeddev.net>
2019-05-26 10:21:59 +0200

    update structure.sql

--- sql/structure.sql
+++ sql/structure.sql
@@ -1,27 +1,29 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
+SET AUTOCOMMIT = 1;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 CREATE TABLE status(
     `id` INT(11) NOT NULL,
     `name` VARCHAR(255) NULL,
-    `order` VARCHAR(255) NULL,
-    `created_at` VARCHAR(255) NOT NULL,
-    `updated_at` VARCHAR(255) NULL
+    `order` ,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE  CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE todo(
-    `id` INT(11) NOT NULL ,
-    `description` VARCHAR(255) NOT NULL,
-    `created_at` VARCHAR(255) NOT NULL,
-    `updated_at` VARCHAR(255) NOT NULL,
+    `id` INT(11) NOT NULL,
+    `name` VARCHAR(255) NULL,
+    `description` VARCHAR(255) NULL,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE  CURRENT_TIMESTAMP,
     `status_id` INT(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/* ADD PRIMARY KEY */
+/* ADD KEY */
 ALTER TABLE `status` ADD PRIMARY KEY (`id`);
 ALTER TABLE `todo` ADD PRIMARY KEY (`id`);
+
 
 /* ACTIVATE AUTO_INCREMENT */
 ALTER TABLE `status`
@@ -32,6 +34,18 @@ ALTER TABLE `todo`
     MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
+/* ADD INDEX */
 ALTER TABLE `todo` ADD INDEX( `status_id`);
 
 ALTER TABLE `todo` ADD FOREIGN KEY (`status_id`) REFERENCES `status`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+/* ADD  */
+
+INSERT INTO `status` (`name`, `order`) VALUES
+('Open', 'unknow'),
+('In Progress', 'unknow'),
+('Done', 'unknow');
+
+INSERT INTO  `todo` (`name`, `description`, `status_id`) VALUES
+('VexedDev Homepage', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam', '1'),
+('Todo app', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam', '1');
